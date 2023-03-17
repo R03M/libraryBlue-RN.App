b2a616059ebe0db36c3f9aae5de379c114f944e8
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-03-17 12:21:36 -0300

    Add extraReducers to checkEmail and loginAccount

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,23 +1,65 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { checkEmail, loginAccount } from "./actions";
 
 const initialState = {
-  user: [],
+  dataUser: [],
   token: null,
+  responseEmail: [],
+  status: "idle",
+  error: [],
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUserReducer: (state, action) => {
-      state.user = action.payload;
+    setUser: (state, action) => {
+      state.dataUser = action.payload;
     },
-    setUserTokenReducer: (state, action) => {
+    setUserToken: (state, action) => {
       state.token = action.payload;
     },
+    deleteResponseEmail: (state) => {
+      state.responseEmail = [];
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+
+      //? checkEmail
+
+      .addCase(checkEmail.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(checkEmail.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.responseEmail = action.payload;
+      })
+      .addCase(checkEmail.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload.message;
+      })
+
+      //? loginAccount
+
+      .addCase(loginAccount.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(
+        loginAccount.fulfilled,
+        (state, { payload: { userData } }) => {
+          state.status = "succeeded";
+          state.dataUser = userData;
+          // state.token = token;
+        }
+      )
+      .addCase(loginAccount.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      });
   },
 });
 
-export const { setUserReducer, setUserTokenReducer } = userSlice.actions;
+export const { setUser, setUserToken, deleteResponseEmail } = userSlice.actions;
 
 export default userSlice.reducer;
