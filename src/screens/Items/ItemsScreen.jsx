a04dcf734d6c8fa520c7cd2b80a32d09092dba6b
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-03-21 19:17:18 -0300

    Update minimal

--- src/screens/Items/ItemsScreen.jsx
+++ src/screens/Items/ItemsScreen.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import { View } from 'react-native';
-import styles from './items.Styles';
+import React from "react";
+import { View, Text, Button } from "react-native";
+import styles from "./items.Styles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const ItemsScreen = () => {
-    return (
-        <View>
-            
-        </View>
-    );
-}
+  const mk = async () => {
+    const token = await AsyncStorage.getItem("@TokenAccess");
+    const userData = await AsyncStorage.getItem("@UserData");
+    console.log(token, userData);
+  };
+
+  return (
+    <View>
+      <Button title="getDataLocal" onPress={mk} />
+    </View>
+  );
+};
 
 export default ItemsScreen;
