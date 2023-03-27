e163043b331e1bc817f450c7c8973fc61ba7a49e
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-03-27 19:47:08 -0300

    Add values for select

---
+++ src/utils/values.enum.js
@@ -0,0 +1,60 @@
+export const editions = [
+  {
+    label: "-",
+    value: null,
+  },
+  {
+    label: "Público",
+    value: "Public",
+  },
+  {
+    label: "Estudio",
+    value: "Study",
+  },
+];
+
+export const letters = [
+  {
+    label: "-",
+    value: null,
+  },
+  {
+    label: "Grande",
+    value: "Big",
+  },
+  {
+    label: "Normal",
+    value: "Normal",
+  },
+];
+
+export const categories = [
+  {
+    label: "-",
+    value: null,
+  },
+  {
+    label: "Revistas",
+    value: "Magazines",
+  },
+  {
+    label: "Libros",
+    value: "Books",
+  },
+  {
+    label: "Folletos",
+    value: "Brochures",
+  },
+  {
+    label: "Guia de Actividades",
+    value: "Activity Guide",
+  },
+  {
+    label: "Tratados",
+    value: "Treaties",
+  },
+  {
+    label: "Otros",
+    value: "Others",
+  },
+];
