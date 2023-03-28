4253b1177698a4f0f8689794e6fe169797b01932
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-03-28 12:29:00 -0300

    Add function for delete spaces in blank and for validate strings of new item

---
+++ src/utils/noBlankSpaces.js
@@ -0,0 +1,5 @@
+const noBlankSpaces = (value) => {
+  return value.trim();
+};
+
+export default noBlankSpaces;
---
+++ src/utils/validateString.js
@@ -0,0 +1,14 @@
+const validateString = (value, name) => {
+  let error;
+  if (value === "") {
+    error = `El ${name} no puede estar en blanco`;
+  } else if (value.length < 4) {
+    error = `El ${name} es muy corto`;
+  }
+
+  if (error) {
+    return error;
+  }
+};
+
+export default validateString;
