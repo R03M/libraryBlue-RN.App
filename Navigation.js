8d2f4cb5cba8df0a5e967091882e2e433aa1d0c0
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-03-29 23:17:32 -0300

    Update style tabBar and icon of profile

--- Navigation.js
+++ Navigation.js
@@ -1,40 +1,37 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { ActivityIndicator, Alert, StatusBar } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { setUser, setUserToken } from "./src/redux/userSlice";
-import { FontAwesome } from "@expo/vector-icons";
-import Loading from "./src/components/Loading";
-import { Entypo, AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
-import sendEmail from "./src/utils/sendEmail.js";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { ActivityIndicator, Image, StatusBar, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { Entypo, AntDesign } from '@expo/vector-icons';
 
 //? Screens
-import ItemsScreen from "./src/screens/Items/ItemsScreen";
-import ProfileScreen from "./src/screens/profile/ProfileScreen";
-import SettingsScreen from "./src/screens/settings/SettingsScreen";
-import RegisterScreen from "./src/screens/register/RegisterScreen";
-import LoginScreen from "./src/screens/login/LoginScreen";
-import useUserData from "./src/hooks/useUserData";
+import ItemsScreen from './src/screens/Items/ItemsScreen';
+import ProfileScreen from './src/screens/profile/ProfileScreen';
+import SettingsScreen from './src/screens/settings/SettingsScreen';
+import RegisterScreen from './src/screens/register/RegisterScreen';
+import LoginScreen from './src/screens/login/LoginScreen';
+import useUserData from './src/hooks/useUserData';
+import ConsoleLoading from './src/components/ConsoleLoading';
 
 const Tab = createBottomTabNavigator();
 
 const MyTabs = () => {
   const userToken = useSelector((state) => state.user.token);
-
+  const imageUser = useSelector((state) => state.user.dataUser.image);
   return (
     <Tab.Navigator initialRouteName="Login" screenOptions={{}}>
-      {userToken === "n/a" ? (
+      {userToken === 'n/a' ? (
         <Tab.Screen
           name="Loading"
-          component={Loading}
+          component={ConsoleLoading}
           options={{
+            tabBarShowLabel: false,
             headerShown: false,
-            tabBarLabel: "",
             tabBarIcon: ({ color, size }) => (
               <ActivityIndicator size={size} color={color} />
             ),
+            tabBarStyle: { display: 'none' },
           }}
         />
       ) : userToken ? (
@@ -43,37 +40,49 @@ const MyTabs = () => {
             name="Item"
             component={ItemsScreen}
             options={{
-              tabBarLabel: "Items",
+              tabBarShowLabel: false,
+              headerShown: false,
               tabBarIcon: ({ color, size }) => (
                 <Entypo name="list" color={color} size={size} />
               ),
-              headerShown: false,
             }}
           />
           <Tab.Screen
             name="Profile"
             component={ProfileScreen}
             options={{
-              tabBarLabel: "Profile",
-              tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons
-                  name="face-man-profile"
-                  size={size}
-                  color={color}
-                />
-              ),
+              tabBarShowLabel: false,
               headerShown: false,
+              tabBarIcon: () => (
+                <View
+                  style={{
+                    height: 40,
+                    width: 40,
+                    borderRadius: 40,
+                    overflow: 'hidden',
+                  }}>
+                  <Image
+                    source={{
+                      uri: imageUser,
+                    }}
+                    style={{
+                      flex: 1,
+                      resizeMode: 'cover',
+                    }}
+                  />
+                </View>
+              ),
             }}
           />
           <Tab.Screen
             name="Settings"
             component={SettingsScreen}
             options={{
-              tabBarLabel: "Settings",
+              tabBarShowLabel: false,
+              headerShown: false,
               tabBarIcon: ({ color, size }) => (
                 <AntDesign name="setting" color={color} size={size} />
               ),
-              headerShown: false,
             }}
           />
         </>
@@ -83,22 +92,22 @@ const MyTabs = () => {
             name="Login"
             component={LoginScreen}
             options={{
-              tabBarLabel: "Login",
+              tabBarShowLabel: false,
+              headerShown: false,
               tabBarIcon: ({ color, size }) => (
                 <Entypo name="login" color={color} size={size} />
               ),
-              headerShown: false,
             }}
           />
           <Tab.Screen
             name="Register"
             component={RegisterScreen}
             options={{
-              tabBarLabel: "Register",
+              tabBarShowLabel: false,
+              headerShown: false,
               tabBarIcon: ({ color, size }) => (
                 <FontAwesome name="sign-in" size={size} color={color} />
               ),
-              headerShown: false,
             }}
           />
         </>
@@ -112,7 +121,7 @@ export default function Navigation() {
 
   return (
     <NavigationContainer>
-      <StatusBar backgroundColor={"#5998c0"} barStyle="default" />
+      <StatusBar backgroundColor={'#5998c0'} barStyle="default" />
       <MyTabs />
     </NavigationContainer>
   );
