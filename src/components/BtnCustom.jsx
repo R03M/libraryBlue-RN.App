00dd11b7e339962f01925beebff31b47227067c5
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-03-29 23:19:38 -0300

    Add doc to component

--- src/components/BtnCustom.jsx
+++ src/components/BtnCustom.jsx
@@ -1,23 +1,39 @@
-import React from "react";
-import { TouchableOpacity, Text } from "react-native";
-
-const BtnCustom = ({ title, onPress, backgroundColor, textColor, styles }) => {
+import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+/**
+ *  Botón personalizado utiliza los componentes TouchableOpacity y Text para renderizar un botón con un texto personalizado y estilos configurables.
+ * @param {string} title El texto que se muestra en el botón.
+ * @param {func} onPrees  La función que se ejecuta cuando se presiona el botón.
+ * @param {string} backgroundColor  El nuevo valor de la propiedad.
+ * @param {string} textColor   El color del texto del botón.
+ * @param {object} styles  los estilos adicionales que se aplican al botón.
+ * @param {object} stylesText    Los estilos adicionales que se aplican al texto del botón.
+ * @returns {void}
+ */
+const BtnCustom = ({
+  title,
+  onPress,
+  backgroundColor,
+  textColor,
+  styles,
+  stylesText,
+}) => {
   return (
     <TouchableOpacity
       style={[
         {
           backgroundColor: backgroundColor,
-          padding: 10,
-          borderRadius: 5,
-          borderWidth: 1,
+          alignItems: 'center',
           borderColor: backgroundColor,
-          margin: 2,
+          padding: 8,
+          borderRadius: 4,
         },
         styles,
       ]}
-      onPress={onPress}
-    >
-      <Text style={{ color: textColor, fontWeight: "bold" }}>{title}</Text>
+      onPress={onPress}>
+      <Text style={[{ color: textColor, fontWeight: 'bold' }, stylesText]}>
+        {title}
+      </Text>
     </TouchableOpacity>
   );
 };
