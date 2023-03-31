63be6ea3c6468b89eb0754210e4d96dd3f3686ff
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-03-30 23:17:56 -0300

    Update

--- src/screens/Items/ItemsScreen.jsx
+++ src/screens/Items/ItemsScreen.jsx
@@ -1,22 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, Button } from 'react-native';
+import { View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-import SearchBar from '../../components/SearchBar';
 import NoCompany from '../../components/NoCompany';
-import NewItem from '../../components/NewItem';
-import styles from './items.Styles';
+import CreateNewItem from '../../components/CreateNewItem';
 import ItemsList from '../../components/ItemsList';
 import { getAllItems } from '../../redux/actions';
 import NavBar from '../../components/NavBar';
+import styles from './items.Styles';
 
 const ItemsScreen = () => {
   const dispatch = useDispatch();
   const { dataUser } = useSelector((state) => state.user);
   const [modalItem, setModalItem] = useState(false);
   const idCompany = useSelector((state) => state.user.dataUser.company.id);
-  const { items, errorSearch, unalterableItems } = useSelector(
-    (state) => state.item
-  );
+  const { items, errorDeleteItem, statusDeleteItem, unalterableItems } =
+    useSelector((state) => state.item);
 
   useEffect(() => {
     if (unalterableItems.length === 0) {
@@ -35,7 +33,7 @@ const ItemsScreen = () => {
         <View style={{ flex: 1 }}>
           <NavBar activeNewItem={() => setModalItem(!modalItem)} />
           <ItemsList data={items} />
-          <NewItem modalItem={modalItem} setModalItem={setModalItem} />
+          <CreateNewItem modalItem={modalItem} setModalItem={setModalItem} />
         </View>
       )}
     </>
