0414c8432bdaefcbcff06dfa1e57a0257766c4e0
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-04-07 23:20:31 -0300

    Update

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,11 +1,15 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const setItems = async (item, payload) => {
+export const lsSetItems = async (item, payload) => {
   await AsyncStorage.setItem(item, JSON.stringify(payload));
 };
-export const getItems = async (item) => {
-  await AsyncStorage.getItem(item);
+
+export const lsGetItems = async (item) => {
+  const itemString = await AsyncStorage.getItem(item);
+  const response = JSON.parse(itemString);
+  return response;
 };
-export const removeItems = async (item, payload) => {
+
+export const lsRemoveItems = async (item) => {
   await AsyncStorage.removeItem(item);
 };
