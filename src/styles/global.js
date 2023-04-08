0858388c8d479a7d4a5434970e0ec62f75db621d
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-04-08 01:28:14 -0300

    Add global styles

---
+++ src/styles/global.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const stylesGlobal = StyleSheet.create({
+  backLight: {
+    backgroundColor: '#EAEAEA',
+  },
+  backDark: {
+    backgroundColor: '#151515',
+  },
+  textLight: {
+    color: '#000',
+  },
+  textDark: {
+    color: '#fff',
+  },
+});
+
+export default stylesGlobal;
