6e1f949855852dea0df72266cd979248c186b292
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-04-10 01:31:56 -0300

    Update

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,13 +1,19 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+// import { encryptedData, decryptData } from './crypto';
+
+export const LS_ITEM = '@Items-lB';
+export const LS_USERDATA = '@UserData-lB';
+export const LS_TOKENACCESS = '@TokenAccess-lB';
+export const LS_CONFIG = '@Settings-lB';
 
 export const lsSetItems = async (item, payload) => {
   await AsyncStorage.setItem(item, JSON.stringify(payload));
 };
 
-export const lsGetItems = async (item) => {
-  const itemString = await AsyncStorage.getItem(item);
-  const response = JSON.parse(itemString);
-  return response;
+export const lsGetItems = async (name) => {
+  const itemStr = await AsyncStorage.getItem(name);
+  const item = JSON.parse(itemStr);
+  return item;
 };
 
 export const lsRemoveItems = async (item) => {
