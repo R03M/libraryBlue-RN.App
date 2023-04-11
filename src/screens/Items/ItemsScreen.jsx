5df7fab3f15a428083d59976b317f2d65b16d54b
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-04-10 21:27:13 -0300

    Add: FeedbackOfAPI

--- src/screens/Items/ItemsScreen.jsx
+++ src/screens/Items/ItemsScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, memo } from 'react';
+import React, { useEffect, memo, useState } from 'react';
 import { View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import NoCompany from '../../components/NoCompany';
@@ -8,30 +8,51 @@ import NavBar from '../../components/NavBar';
 import { useTheme } from '../../hooks/useTheme';
 import styles from './items.Styles';
 import stylesGlobal from '../../styles/global';
+import FeedbackOfAPI from '../../components/FeedbackOfAPI';
 
 const ItemsScreen = () => {
   const dispatch = useDispatch();
   const isDarkTheme = useTheme();
+  
+  const [feedbackActive, setFeedbackActive] = useState(false);
+
   const { dataUser, token } = useSelector((state) => state.user);
-  const { items } = useSelector((state) => state.item);
+  const { items, unalterableItems } = useSelector((state) => state.item);
+  const { statusUpdateItem } = useSelector((state) => state.item);
 
   useEffect(() => {
     if (dataUser.company) {
-      const getItems = () => {
-        let idCompany = dataUser.company.id;
-        dispatch(
-          getAllItems({
-            idCompany,
-            idAssociated: dataUser.company.associatedCompany
-              ? dataUser.company.associatedCompany
-              : null,
-            token,
-          })
-        );
-      };
-      getItems();
+      if (unalterableItems.length === 0) {
+        const getItems = () => {
+          let idCompany = dataUser.company.id;
+          dispatch(
+            getAllItems({
+              idCompany,
+              idAssociated: dataUser.company.associatedCompany
+                ? dataUser.company.associatedCompany
+                : null,
+              token,
+            })
+          );
+        };
+        getItems();
+      }
     }
-  }, []);
+  }, [dataUser]);
+
+  useEffect(() => {
+    if(statusUpdateItem === 'loading'){
+      setFeedbackActive(true)
+    }
+    if (
+      statusUpdateItem === 'succeeded' ||
+      statusUpdateItem === 'failed'
+    ) {
+      setTimeout(() => {
+        setFeedbackActive(false);
+      }, 800);
+    }
+  }, [statusUpdateItem]);
 
   return (
     <View
@@ -43,10 +64,15 @@ const ItemsScreen = () => {
       {!dataUser.company ? (
         <NoCompany />
       ) : (
-        <>
+        <View style={{ flex: 1, width: '100%' }}>
           <NavBar />
           <ItemsList data={items} idCompany={dataUser.company.id} />
-        </>
+          {feedbackActive && (
+            <View style={stylesGlobal.feedbackContainer}>
+              <FeedbackOfAPI value={statusUpdateItem} />
+            </View>
+          )}
+        </View>
       )}
     </View>
   );
