6456d21c9b0d96812d6d967708981cec70f47785
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-04-22 13:11:13 -0300

    Add: alert in case of not items

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,17 @@
-import { TextInput } from 'react-native';
+import { Alert, TextInput } from 'react-native';
 import { StyleSheet } from 'react-native';
 
-const SearchBar = ({ search, handlerSearch }) => {
+const SearchBar = ({ search, handlerSearch, areThereArt }) => {
   return (
     <TextInput
-      onChangeText={handlerSearch}
+      onChangeText={
+        areThereArt.length > 0
+          ? handlerSearch
+          : () =>
+              Alert.alert(null, 'No tienes items en los que buscar', [], {
+                cancelable: true,
+              })
+      }
       value={search}
       placeholder={`Encuentra lo que necesitas`}
       style={styles.search}
